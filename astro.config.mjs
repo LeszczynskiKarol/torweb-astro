dcf154e5d3eba8db1e745b63e814d23bcaa93ee3
LeszczynskiKarol <leszczu123@wp.pl>
2026-02-11 13:50:01 +0100

    poprawka seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://torweb.pl",
+  site: "https://www.torweb.pl",
   integrations: [tailwind(), sitemap()],
   compressHTML: true,
 });
